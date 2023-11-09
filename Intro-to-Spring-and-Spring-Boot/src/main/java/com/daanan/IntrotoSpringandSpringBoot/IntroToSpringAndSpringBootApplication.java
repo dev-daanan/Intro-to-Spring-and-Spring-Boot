2d188fcee4b1e22591e7937e6249200252dad434
Daanan Dasilva <daanandasilva@gmail.com>
2023-11-09 12:37:37 -0700

    Initial Commit

---
+++ Intro-to-Spring-and-Spring-Boot/src/main/java/com/daanan/IntrotoSpringandSpringBoot/IntroToSpringAndSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.daanan.IntrotoSpringandSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroToSpringAndSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroToSpringAndSpringBootApplication.class, args);
+	}
+
+}
---
+++ Intro-to-Spring-and-Spring-Boot/src/test/java/com/daanan/IntrotoSpringandSpringBoot/IntroToSpringAndSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daanan.IntrotoSpringandSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroToSpringAndSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
